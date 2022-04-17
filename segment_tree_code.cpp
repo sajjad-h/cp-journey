48feaee687b2ae2519dd721dd3696ebc7b208ea8
Sajjad Hossain <59442218+sajjad-h@users.noreply.github.com>
2022-04-18 01:51:32 +0600

    CF 1659D code added

--- segment_tree_code.cpp
+++ segment_tree_code.cpp
@@ -1,127 +1,105 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define   ll     long long int
+#define    ll    long long int
 
-const int N = 100005;
-ll tree[4*N];
-ll lazy[4*N];
-ll ara[N];
-ll n, x, y;
+const int N = 200000;
+ll tree[4*N + 5];
+ll lazy[4*N + 5];
+ll ara[N + 5];
 
-void build(int node, int start, int end)
+void segBuild(int node, int s, int e)
 {
-    int left = node << 1;
-    int right = left | 1;
-    if(start == end)
-    {
-    	lazy[node] = -1;
-        tree[node] = ara[start];
+    if (s == e) {
+        tree[node] = ara[s];
+        return ;
     }
-    else
-    {
-        int mid = (start + end) / 2;
-        build(left, start, mid);
-        build(right, mid+1, end);
-    	lazy[node] = -1;
-        tree[node] = tree[left] + tree[right];
-    }
-}
-
-void updateRange(int node, int start, int end, int l, int r, ll val)
-{
-    int mid = (start + end) / 2;
-    int left = node << 1;
-    int right = left | 1;
-    if(lazy[node] != -1)
-    {
-        tree[node] = (end - start + 1) * lazy[node];
-        if(start != end)
-        {
-            lazy[left] = lazy[node];
-            lazy[right] = lazy[node];
-        }
-        lazy[node] = -1;
-    }
-    if(start > end or start > r or end < l)
-        return;
-    if(start >= l and end <= r)
-    {
-        tree[node] = (end - start + 1) * val;
-        if(start != end)
-        {
-            lazy[left] = val;
-            lazy[right] = val;
-        }
-        return;
-    }
-    updateRange(left, start, mid, l, r, val);
-    updateRange(right, mid + 1, end, l, r, val);
+    int mid = (s + e) / 2;
+    int left = 2 * node;
+    int right = left + 1;
+    segBuild(left, s, mid);
+    segBuild(right, mid + 1, e);
     tree[node] = tree[left] + tree[right];
 }
 
-ll queryRange(int node, int start, int end, int l, int r)
+void segUpdate(int node, int s, int e, int l, int r, int val)
 {
-    int mid = (start + end) / 2;
-    int left = node << 1;
-    int right = left | 1;
-    if(start > end or start > r or end < l)
-        return 0LL;
-    if(lazy[node] != -1) {
-        tree[node] = (end - start + 1) * lazy[node];
-        if(start != end) {
-            lazy[left] = lazy[node];
-            lazy[right] = lazy[node];
+    int mid = (s + e) / 2;
+    int left = 2 * node;
+    int right = left + 1;
+    if (lazy[node] != 0) {
+        tree[node] += (e - s + 1) * lazy[node];
+        if (s != e) {
+            lazy[left] += lazy[node];
+            lazy[right] += lazy[node];
         }
-        lazy[node] = -1;
+        lazy[node] = 0;
     }
-    if(start >= l and end <= r) {
-        return tree[node];
+    if (s > e || r < s || e < l) return ;
+    if (l <= s && e <= r) {
+        tree[node] += (e - s + 1) * val;
+        if (s != e) {
+            lazy[left] += val;
+            lazy[right] += val;
+        }
+        return ;
     }
-    ll p1 = queryRange(left, start, mid, l, r);
-    ll p2 = queryRange(right, mid + 1, end, l, r);
-    return (p1 + p2);
+    segUpdate(left, s, mid, l, r, val);
+    segUpdate(right, mid + 1, e, l, r, val);
+    tree[node] = tree[left] + tree[right];
 }
 
-void solve()
+ll segQuery(int node, int s, int e, int l, int r)
 {
-	int n, q; cin >> n >> q;
-	vector<int> a(n + 1);
-	vector<int> pos(n + 1, -1);
-	for (int i = 1; i <= n; i++) {
-		cin >> a[i];
-	}
-	vector<ll> ans(n + 2, 0);
-	for (int i = 1; i <= n; i++) {
-		if (pos[a[i]] == -1) {
-			ans[i] += i;
-			ans[n + 1] -= i;
-		} else {
-			ans[i] += (i - pos[a[i]]);
-			ans[n + 1] -= (i - pos[a[i]]);
-		}
-		pos[a[i]] = i;
-	}
-	for (int i = 1; i <= n; i++) {
-		ans[i] += ans[i - 1];
-	}
-	for (int i = 1; i <= n; i++) {
-		ans[i] += ans[i - 1];
-	}
-	for (int i = 0; i < q; i++) {
-		int type, k; cin >> type >> k;
-		if (type == 2) {
-			cout << ans[k] << '\n';
-		}
-	}
+    int mid = (s + e) / 2;
+    int left = 2 * node;
+    int right = left + 1;
+    if (lazy[node] != 0) {
+        tree[node] += (e - s + 1) * lazy[node];
+        if (s != e) {
+            lazy[left] += lazy[node];
+            lazy[right] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+    if (s > e || r < s || e < l) return 0;
+    if (l <= s && e <= r) {
+        return tree[node];
+    }
+    return (segQuery(left, s, mid, l, r) + segQuery(right, mid + 1, e, l, r));
+}
+
+void testcase()
+{
+    int n; cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> ara[i];
+    }
+    segBuild(1, 1, n);
+    int ones = tree[1] / n;
+    vector<int> ans(n + 1, 0);
+    for (int i = n; ones > 0 && i >= 1; i--) {
+        segUpdate(1, 1, n, i - ones + 1, i, -1);
+        ll a = segQuery(1, 1, n, i, i);
+        if (a > 0) {
+            ans[i] = 1;
+            ones--;
+        }
+    }
+    if (ones > 0) {
+        ans[1] = 1;
+    }
+    for (int i = 1; i <= n; i++) {
+        cout << ans[i] << ' ';
+    }
+    cout << '\n';
 }
 
 int main()
 {
-	int t = 1; //cin >> t;
-	while (t--) {
-		solve();
-	}
-	return 0;
+    int t; cin >> t;
+    while (t--) {
+        testcase();
+    }
+    return 0;
 }
-
