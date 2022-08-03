22497a95bd2cd3ac84d9b40c6520324d148b78bb
sajjad-h <hoosain.sajjad@gmail.com>
2022-08-03 11:47:24 +0600

    unique random array generator added

--- RANDOM_ARRAY_GENERATOR.cpp
+++ RANDOM_ARRAY_GENERATOR.cpp
@@ -2,6 +2,29 @@
 using namespace std;
 
 
+int contains(vector<int> v, int sz, int x)
+{
+    for (int i = 0; i < sz; i++) {
+        if (v[i] == x) return 1;
+    }
+    return 0;
+}
+
+vector<int> unique_random_array(int n, int lo, int hi, int uni = 0)
+{
+	//Random Number Generator
+	mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+    vector<int> permutation(n);
+    for (int i = 0; i < n; i++) {
+        permutation[i] = rand() % (hi - lo + 1) + lo;
+        while(uni && contains(permutation, i, permutation[i])) {
+            permutation[i] = rand() % (hi - lo + 1) + lo;
+        }
+    }
+    shuffle(permutation.begin(), permutation.end(), rng);
+    return permutation;
+}
+
 vector<int> random_array(int N)
 {
 	//Random Number Generator
